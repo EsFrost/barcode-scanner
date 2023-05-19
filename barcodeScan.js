ba11c86b3e5792da1bb6e189c4e8df844668663f
Sigmund Frost <sigmafi.tech@gmail.com>
2023-05-19 10:08:54 +0300

    Fixed annoying bug where sales were being multiplied

--- barcodeScan.js
+++ barcodeScan.js
@@ -1,5 +1,3 @@
-// on print recalculate if last saved id is equal to new and there are still products on the screen
-
 const input = document.getElementById('input')
 const productResult = document.getElementById('productResult')
 const sales = document.getElementById('sales')
@@ -8,19 +6,12 @@ const returnBtn = document.getElementById('return')
 const search = document.getElementById('search')
 const colorMode = document.getElementById('colorMode')
 const subTotal = document.getElementById('subtotal')
+const modalBtn = document.getElementById('modal-btn')
 let newDiv = document.createElement('div')
 let subTotalPrice = 0.0
 let rowsCopy, total
 let cached = []
 let buttonFlag = false
-
-const createNewDiv = (price) => {
-	newDiv.setAttribute('id', 'subTotalPrint')
-	newDiv.textContent = `Σύνολο: ${price} €`
-	document.body.appendChild(newDiv)
-}
-
-createNewDiv(subTotalPrice)
 
 if (localStorage.getItem('colorMode') === null) {
 	localStorage.setItem('colorMode', 'dark')
@@ -38,16 +29,6 @@ else {
 }
 
 subTotal.addEventListener('click', () => {
-	buttonFlag = true
-	subTotalPrice = 0.0
-	if (document.getElementById('subTotalPrint')) {
-		document.getElementById('subTotalPrint').textContent = `0`
-	}
-
-	/*if (!document.getElementById('productResult').firstChild) {
-		createNewDiv(subTotalPrice)
-		return
-	}*/
 	
 	if (cached.length !== 0) {
 		cached[cached.length-1][3] = true
@@ -60,24 +41,37 @@ subTotal.addEventListener('click', () => {
 			localStorage.setItem('cachedPrice', JSON.stringify(temp))
 		}
 
+		modalBtn.parentElement.parentElement.parentElement.style.display = 'flex'
 
-		let reverseTotal = [...JSON.parse(localStorage.getItem('cachedPrice')).slice(0).reverse()]
-		let flag = 0
-		reverseTotal.map((item) => {
-			if (item[3] === true) {
-				flag++
-			}
-			if (flag <= 1) {
-				subTotalPrice += item[2]
-			}
-			else if (flag >= 2 ) {
-				return
-			}
+		cached.map((item) => {
+			subTotalPrice = subTotalPrice + item[2]
+			const resDiv = document.createElement('div')
+			const resSpan1 = document.createElement('span')
+			const resSpan2 = document.createElement('span')
+
+			resDiv.classList.add('result-div')
+			resSpan1.classList.add('text-lg', 'productName')
+			resSpan2.classList.add('text-lg', 'productPrice')
+
+			resSpan1.textContent = `${item[1]} :`
+			resSpan2.textContent = `${item[2]} €`
+
+			resDiv.appendChild(resSpan1)
+			resDiv.appendChild(resSpan2)
+
+			document.getElementById('modal-products').appendChild(resDiv)
+
 		})
-		
+		subTotalPrice = parseFloat(subTotalPrice.toString().replace(',', '.'))
+		document.getElementById('modal-subtotal').textContent = `Σύνολο: ${subTotalPrice.toFixed(1)} €`
+
+		modalBtn.addEventListener('click', () => {
+			modalBtn.parentElement.parentElement.parentElement.style.display = 'none'
+			location.reload()
+		})
+
 	}
-	subTotalPrice = parseFloat(subTotalPrice.toString().replace(',', '.'))
-	document.getElementById('subTotalPrint').textContent = `Σύνολο: ${subTotalPrice.toFixed(1)} €`
+	
 
 	cached = []
 	subTotalPrice = 0.0
@@ -126,39 +120,38 @@ const productElement = (name, price, idVal, falseVal) => {
 	resButton.addEventListener('click', (e) => {
 		resButton.parentElement.remove()
 		let finalArr = []
-		if (JSON.parse(localStorage.getItem('cachedPrice')) === null) {
-			return
+		if (cached.length !== 0) {
+			cached.map((item) => {
+				if (item[0] !== resButton.parentElement.getAttribute('id')) {
+					finalArr.push(item)
+				}
+			})
+			cached = finalArr
+			if (cached.length === 0) {
+				location.reload()
+			}
+
+			finalArr = []
 		}
-		else {
+
+		if (JSON.parse(localStorage.getItem('cachedPrice')).length !== 0) {
 			JSON.parse(localStorage.getItem('cachedPrice')).map((item) => {
 				if (item[0] !== resButton.parentElement.getAttribute('id')) {
 					finalArr.push(item)
 				}
 			})
+
 			localStorage.setItem('cachedPrice', JSON.stringify(finalArr))
+
+			finalArr = []
 		}
-	})
-}
-
-const warningElement = (message) => {
-	const warningDiv = document.createElement('h1')
-
-	warningDiv.classList.add('text-lg', 'warning')
-
-	warningDiv.setAttribute('id', 'warning')
-
-	warningDiv.textContent = message
-
-	productResult.appendChild(warningDiv)
-
-	warningDiv.addEventListener('click', (e) => {
-		productResult.firstChild.remove()
 	})
 }
 
 clearCache.addEventListener('click', () => {
 	localStorage.removeItem('cachedPrice')
 	total = 0
+	cache = []
 	location.reload()
 })
 
@@ -201,7 +194,8 @@ sales.addEventListener('click', () => {
 		productResult.appendChild(totalDiv)
 	}
 	else {
-		warningElement('Δεν έχει γίνει κάποια πώληση!')
+		document.getElementById('warning').textContent = 'Δεν έχει γίνει κάποια πώληση!'
+		document.getElementById('alert').style.display = 'block'
 		total = 0
 	}
 })
@@ -215,8 +209,6 @@ returnBtn.addEventListener('click', () => {
 	document.getElementById('productResult').style['position'] = 'inherit'
 	subTotal.style['position'] = 'inherit'
 
-	document.getElementById('subTotalPrint').textContent = `Σύνολο: 0 €`
-	document.getElementById('subTotalPrint').style['position'] = 'inherit'
 	location.reload()
 
 })
@@ -234,10 +226,6 @@ input.addEventListener('click', (e) => {
 
 
 search.addEventListener('keypress', (e) => {
-	if (buttonFlag === true && document.getElementById('productResult').firstChild) {
-		buttonFlag = 0
-		document.getElementById('productResult').textContent = ''
-	}
 	if (e.key === 'Enter') {
 
 		if (localStorage.getItem('arrayCopy')) {
@@ -248,7 +236,8 @@ search.addEventListener('keypress', (e) => {
 				}
 			})
 
-			if (result[0]) {
+
+			if (result.length !== 0) {
 				let id = Math.random().toString(16).substr(2, 12)
 
 				const test = [id, result[0][2], result[0][1], false]
@@ -265,7 +254,8 @@ search.addEventListener('keypress', (e) => {
 			}
 		}
 		else {
-			warningElement('Δεν έχει επιλεγεί αρχείο xlxs')
+			document.getElementById('warning').textContent = 'Δεν έχει επιλεγεί αρχείο xlxs!'
+			document.getElementById('alert').style.display = 'block'
 		}
 
 		search.value = ''
